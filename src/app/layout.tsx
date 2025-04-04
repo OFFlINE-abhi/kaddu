85b1d0785bca2de9ce265bb1ea43b2613a1dbf97
OFFlINE-abhi <offline5151@gmail.com>
2025-04-04 23:49:12 +0530

    🔥 Fix: Firebase key + 404 route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "./component/Navbar";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
